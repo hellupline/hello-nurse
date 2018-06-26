aaea614932e99ad6490038dcc447e60826bfeef4
Renan Traba <hellupline@gmail.com>
2018-06-26 08:52:01 -0300

    restore old push code

--- nursefetch/go.mod
+++ nursefetch/go.mod
@@ -1,3 +1,18 @@
 module github.com/hellupline/hello-nurse/nursefetch
 
-require github.com/streadway/amqp v0.0.0-20180528204448-e5adc2ada8b8
+require (
+	github.com/golang/protobuf v1.1.0
+	github.com/onsi/ginkgo v1.5.0
+	github.com/onsi/gomega v1.4.0
+	github.com/sirupsen/logrus v1.0.5
+	github.com/streadway/amqp v0.0.0-20180528204448-e5adc2ada8b8
+	github.com/stretchr/testify v1.2.2
+	golang.org/x/crypto v0.0.0-20180621125126-a49355c7e3f8
+	golang.org/x/net v0.0.0-20180621144259-afe8f62b1d6b
+	golang.org/x/sync v0.0.0-20180314180146-1d60e4601c6f
+	golang.org/x/sys v0.0.0-20180625162522-a200a19cb90b
+	golang.org/x/text v0.3.0
+	gopkg.in/airbrake/gobrake.v2 v2.0.9
+	gopkg.in/gemnasium/logrus-airbrake-hook.v2 v2.1.2
+	gopkg.in/yaml.v2 v2.2.1
+)
--- nursefetch/main.go
+++ nursefetch/main.go
@@ -2,22 +2,61 @@ package main
 
 import (
 	"encoding/json"
-	"log"
+	"encoding/xml"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
+	"time"
 
+	log "github.com/sirupsen/logrus"
 	"github.com/streadway/amqp"
 )
 
 type (
+	PipelineCallback func(*TagPage, chan<- *TagPage)
+	DomainURL        func(*TagPage) *url.URL
+
 	NurseFetchTask struct {
 		Domain string `json:"domain" binding:"required"`
 		Tag    string `json:"tag" binding:"required"`
 	}
+
+	TagPage struct {
+		Domain string `xml:"-"`
+		Name   string `xml:"-"`
+		Page   int    `xml:"-"`
+		Limit  int    `xml:"-"`
+
+		Count int         `xml:"count,attr"`
+		Posts []*PostPage `xml:"post"`
+
+		Logger *log.Entry `xml:"-"`
+	}
+	PostPage struct {
+		URL     string `xml:"file_url,attr"`
+		Key     string `xml:"id,attr"`
+		RawTags string `xml:"tags,attr"`
+	}
 )
 
 var (
+	domainURLs = map[string]DomainURL{
+		"konachan.net": KonachanUrlBuilder,
+	}
+
 	amqpConnection *amqp.Connection
 	amqpChannel    *amqp.Channel
 	nurseQueue     amqp.Queue
+
+	uploadURL      = "http://nursequery:8080/v1/dataset/upload/gob"
+	healthCheckURL = "http://nursequery:8080/_ah/health"
+
+	waitPeriod time.Duration = 10
 )
 
 func main() {
@@ -56,7 +95,9 @@ func main() {
 		for d := range msgs {
 			data := NurseFetchTask{}
 			json.Unmarshal(d.Body, &data)
-			log.Printf("Received a message: %#v", data)
+
+			tagPage := NewTagPage(data.Domain, data.Tag)
+			log.Printf("Received a message: %#v", tagPage)
 		}
 	}()
 
@@ -66,8 +107,108 @@ func main() {
 	<-forever
 }
 
+// TagPage API
+
+func NewTagPage(domain, name string) *TagPage {
+	return &TagPage{
+		Domain: domain,
+		Name:   name,
+		Page:   0,
+		Limit:  100,
+
+		Logger: log.WithFields(log.Fields{
+			"domain": domain,
+			"name":   name,
+			"page":   0,
+			"limit":  100,
+		}),
+	}
+
+}
+
+func (t *TagPage) Fetch() error {
+	if t.Page < 1 {
+		t.CreateCacheDir()
+	}
+	filename := t.Filename()
+	body, err := ioutil.ReadFile(filename)
+	if err != nil {
+		u := t.URL().String()
+		resp, err := http.Get(u)
+		if err != nil {
+			return err
+		}
+		defer resp.Body.Close() // nolint: errcheck
+
+		body, err = ioutil.ReadAll(resp.Body)
+		if err != nil {
+			return err
+		}
+		err = ioutil.WriteFile(filename, body, 0660)
+		if err != nil {
+			return err
+		}
+	}
+
+	return xml.Unmarshal(body, t)
+}
+
+func (t *TagPage) NextPage(page int) *TagPage {
+	return &TagPage{
+		Domain: t.Domain,
+		Name:   t.Name,
+		Page:   page,
+		Limit:  t.Limit,
+
+		Logger: log.WithFields(log.Fields{
+			"domain": t.Domain,
+			"name":   t.Name,
+			"page":   page,
+			"limit":  t.Limit,
+		}),
+	}
+
+}
+
+func (t *TagPage) Filename() string {
+	basePath := "/data" // XXX: use envvar
+	prefix := filepath.Join(basePath, "cache", t.Domain, t.Name)
+	return filepath.Join(prefix, fmt.Sprintf("%04d.xml", t.Page))
+}
+
+func (t *TagPage) CreateCacheDir() {
+	basePath := "/data" // XXX: use envvar
+	prefix := filepath.Join(basePath, "cache", t.Domain, t.Name)
+	os.MkdirAll(prefix, os.ModePerm)
+}
+
+func (t *TagPage) URL() *url.URL {
+	return domainURLs[t.Domain](t)
+}
+
+func (t *TagPage) Pages() int {
+	return (t.Count / t.Limit) + 1
+}
+
+func (p *PostPage) Tags() []string {
+	return strings.Split(strings.TrimSpace(p.RawTags), " ")
+}
+
 func failOnError(err error, msg string) {
 	if err != nil {
 		log.Fatalf("%s: %s", msg, err)
+	}
+}
+
+func KonachanUrlBuilder(tag *TagPage) *url.URL {
+	return &url.URL{
+		RawQuery: url.Values{
+			"limit": []string{strconv.Itoa(tag.Limit)},
+			"page":  []string{strconv.Itoa(tag.Page)},
+			"tags":  []string{tag.Name},
+		}.Encode(),
+		Scheme: "https",
+		Host:   "konachan.net",
+		Path:   "/post.xml",
 	}
 }

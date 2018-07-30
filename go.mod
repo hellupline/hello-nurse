cc00b6c0a53c7df8a8e46a61a7d784fe7e8df405
Renan Traba <hellupline@gmail.com>
2018-07-30 02:08:06 -0300

    add thumbnails to selected queue

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ require (
 	github.com/davecgh/go-spew v1.1.0 // indirect
 	github.com/fsnotify/fsnotify v1.4.7 // indirect
 	github.com/go-chi/chi v3.3.2+incompatible
+	github.com/go-chi/cors v1.0.0
 	github.com/go-chi/render v1.0.1
 	github.com/golang/protobuf v1.1.0 // indirect
 	github.com/hpcloud/tail v1.0.0 // indirect
--- main.go
+++ main.go
@@ -11,6 +11,7 @@ import (
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/cors"
 	"gopkg.in/go-playground/pool.v3"
 )
 
@@ -48,12 +49,22 @@ func main() {
 	p := pool.NewLimited(8)
 	defer p.Close()
 
+	cors := cors.New(cors.Options{
+		AllowedOrigins:   []string{"*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	})
+
 	r := chi.NewRouter()
 
 	r.Use(middleware.RequestID)
 	r.Use(middleware.RealIP)
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
+	r.Use(cors.Handler)
 
 	r.Route("/v1", func(r chi.Router) {
 		r.Mount("/posts", PostsResource{}.Routes())
